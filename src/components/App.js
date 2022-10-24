c231f2f9da1eb4d067366a91bfeae14e29177ea1
mercuri <mercuri90028@gmail.com>
2022-10-24 17:06:39 +0530

    component id doen

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,59 @@
-import React from 'react'
+import React,{useRef, useState} from 'react';
 import '../styles/App.css';
+
 import { IgnoreCaseToggle } from './IgnoreCaseToggle';
 import { ParagraphInput } from './ParagraphInput';
 import { WordInput } from './WordInput';
+
+
 const App = () => {
+
+  const [word, setWord] = useState("");
+  const [wordCount, setWordCount] = useState(0);
+  const [ignoreCase, setIgnoreCase] = useState(false);
+
+  const ref = useRef();
+
+  const highlight = (val, wordCase) => {
+    let newt = '';
+    let i = 0;
+
+    if(wordCase) {
+      const reg = new RegExp(val, 'ig')
+      newt = ref.current.innerHTML.replaceAll(reg, (args) => {
+        i++;
+        return `<span class="highlighted-txt">${args}</span>`
+      })
+    } else{
+      newt = ref.current.innerText.replaceAll(val, (args) => {
+        i++;
+        return `<span class="highlighted-txt">${args}</span>`
+      })
+    }
+
+    setWordCount(i);
+
+    ref.current.innerHTML = newt;
+  }
+
+  const handleWordInput = (val) => {
+    highlight(val,ignoreCase)
+    setWord(val)
+  }
+
+  const handleChangeToggle = () =>{
+    highlight(word,!ignoreCase)
+    setIgnoreCase(!ignoreCase)
+  }
+
   return (
     <div id="main">
-      <ParagraphInput />
-      <WordInput/>
+      <ParagraphInput pRef={ref}/>
+      <div>Total matches:- <span id="words-counter">{wordCount}</span></div>
+      <WordInput handleChnage = {handleWordInput} value={word}/>
       <br />
-      <IgnoreCaseToggle/>
+      <IgnoreCaseToggle checked={ignoreCase} onChange={handleChangeToggle}/>
+      <span id="words-counter">{wordCount}</span>
     </div>
   )
 }
--- src/components/WordInput.js
+++ src/components/WordInput.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-export const WordInput = () => {
+export const WordInput = ({handleChnage, value}) => {
     return (
         <>   
             <label htmlFor="word-input">Enter word to be highlighted here</label>
-            <input id="word-input" type={"text"}/>
+            <input 
+                id="word-input" 
+                type={"text"}
+                onChange={(e)=>handleChnage(e.target.value)}
+                value={value}
+            />
         </>
     )
 }
